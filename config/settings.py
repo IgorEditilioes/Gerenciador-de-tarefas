77b1ba0188a8485fcdc19dee8b73426b8194daca
Igor Faria Araujo Editiliões <igor.editilioes@triauto.local>
2026-07-31 17:02:02 -0300

    adicionando hosts

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-!6gg3!mj4nouvib%uat46scm90ttbmj54ud7x(*b^)agfwp-6p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    '192.168.2.16'
+]
 
 
 # Application definition
